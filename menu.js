0f3c6c802c6f61019b5089e75d161bec0967318a
TheisSJacobsen <siehtnesbocaj@gmail.com>
2015-04-27 14:10:50 +0200

    dfsdofj

--- menu.js
+++ menu.js
@@ -13,16 +13,20 @@ var SplashScreen = function(ctx){
 		// console.log('save game')
 		ctx.fillRect(0,280,W,30);
 	if(position_of_mouse.y>=315&&position_of_mouse.y<340)
-		// console.log('Credits')
+		// console.log('load game')
 		ctx.fillRect(0,305,W,30);
+	if(position_of_mouse.y>=340&&position_of_mouse.y<365)
+		// console.log('load game')
+		ctx.fillRect(0,330,W,30);
 	ctx.fillStyle = 'White';
 	ctx.font = '30px Verdana, sans-serif';
 	ctx.textBaseline = 'top';
 
 	ctx.textAlign = 'left';
-	ctx.fillText('New Game' , 350, 250);
-	ctx.fillText('Save Game', 350, 275);
-	ctx.fillText('Credits'	, 350, 300);
+	ctx.fillText('New Game' 	, 350, 250);
+	ctx.fillText('Save Game'	, 350, 275);
+	ctx.fillText('Load Game'	, 350, 300);
+	ctx.fillText('Credits'		, 350, 325);
 	// ctx.fillStyle = color;
 	// ctx.font = '24px Verdana, sans-serif';
 	// ctx.fillText('click to begin', 450, 250+ 30);
